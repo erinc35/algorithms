afea82d34059ed917dc3a338109a477e1c38c5a8
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-02-20 15:39:35 -0800

    max_consecutive_ones added

--- algorithm.js
+++ algorithm.js
@@ -68,3 +68,26 @@ function convert(n){
 
   return result
 }
+
+// Given a binary array, find the maximum number of consecutive 1s in this array.
+
+// Example 1:
+// Input: [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s.
+//     The maximum number of consecutive 1s is 3.
+
+function max_consecutive_ones(input){
+  var max = 0;
+  var current = 0;
+
+  for(var i=0; i<input.length; i++){
+    if(input[i] === 1){
+      current++;
+      if (current > max) max = current;
+    }else{
+      current = 0
+    }
+  }
+  return max
+}
