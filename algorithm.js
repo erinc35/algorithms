b1ece6007b16ac154ac99a43e59734c3d010aef0
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-02-23 10:55:42 -0800

    selection sort added

--- algorithm.js
+++ algorithm.js
@@ -337,5 +337,21 @@ function latticePaths(n, i, j) {
   return latticePaths(n, i + 1, j) + latticePaths(n, i, j + 1);
 }
 
+var selectionSort = function(array){
+  for(var i = 0; i < array.length; i++){
+    //set min to the current iteration of i
+    var min = i;
+    for(var j = i+1; j < array.length; j++){
+      if(array[j] < array[min]){
+       min = j;
+      }
+    }
+    var temp = array[i];
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+};
+var array = [3,2,10,1]
 
 
