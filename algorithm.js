0d4602d0cea72876863c523587038dad98b4a218
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-02-24 10:46:56 -0800

    palindrome algorithm added

--- algorithm.js
+++ algorithm.js
@@ -354,4 +354,13 @@ var selectionSort = function(array){
 };
 var array = [3,2,10,1]
 
+function palindrome(str){
+  var len = Math.floor(str.length/2);
+  for(var i = 0; i<len; i++){
+    if(str[i] !== str[str.length - i - 1]){
+      return false
+    }
+    return true
+  }
+}
 
