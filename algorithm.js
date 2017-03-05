555693b3b0e7c4936096cc86f1cd65153db86cf7
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-03-05 08:43:56 -0800

    isUgly algo added

--- algorithm.js
+++ algorithm.js
@@ -599,4 +599,25 @@ var searchInsert = function(nums, target) {
 
 searchInsert(nums, 3)
 
+// Write a program to check whether a given number is an ugly number.
+
+// Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+// Note that 1 is typically treated as an ugly number.
+
+var isUgly = function(num) {
+    while(num >= 2) {
+        if(num%2 === 0) {
+            num /= 2;
+        } else if(num%3 === 0) {
+            num /= 3;
+        } else if(num%5 === 0) {
+            num /= 5;
+        } else {
+            return false;
+        }
+    }
+
+    return num === 1;
+};
 
