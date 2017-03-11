ccbe5d5ea60a448767a93fd4aef16dedfd7e3d58
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-03-11 11:54:23 -0800

    shortest anagram added

--- algorithm.js
+++ algorithm.js
@@ -722,32 +722,25 @@ var containsNearbyDuplicate = function(nums, k) {
     }
     return false;
 };
-var isAnagram = function(s, t) {
-   var slen = s.length;
-   var tlen = t.length;
+WordDistance.prototype.shortest = function(word1, word2) {
+    var i = 0;
+    var j = 0;
+    var dist = Infinity;
+    var pos1 = this.positions[word1];
+    var pos2 = this.positions[word2];
 
-   if(slen !== tlen) {
-       return false;
-   }
+    while(i < pos1.length && j < pos2.length) {
+        var i1 = pos1[i];
+        var i2 = pos2[j];
 
-   var hash = {};
+        dist = Math.min(dist, Math.abs(i1 - i2));
 
-   for(var i = 0; i < slen; i++) {
-       var char = s[i];
-       hash[char] = hash[char] || 0;
-       hash[char]++;
-      console.log(hash)
-   }
+        if(i1 < i2) {
+            i++;
+        } else {
+            j++;
+        }
+    }
 
-   for(i = 0; i < tlen; i++) {
-       char = t[i];
-
-       if(hash[char] === undefined || hash[char] === 0) {
-           return false;
-       }
-
-       hash[char]--;
-   }
-
-   return true;
+    return dist;
 };
