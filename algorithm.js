4910b30573b83522c15a8a8247714cd8e9a75e1e
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-03-20 21:26:51 -0700

    findPeakElement algo added

--- algorithm.js
+++ algorithm.js
@@ -827,28 +827,28 @@ function solution(A, B, M, X, Y) {
 
 solution(A, B, M, X, Y)
 
-var solution = function(knows) {
-    /**
-     * @param {integer} n Total people
-     * @return {integer} The celebrity
-     */
-    return function(n) {
-        let candidate = 0;
+var findPeakElement = function(nums) {
+    var len = nums.length,
+        left,
+        right,
+        mid;
 
-        for (let i = 1; i < n; i++) {
-            // if candidate know i, then swap
-            if (knows(candidate, i)) {
-                candidate = i;
-            }
+    if (len === 1) {
+        return 0;
+    }
+
+    left = 0;
+    right = len - 1;
+    while(left < right) {
+        mid = parseInt((left + right)/2);
+
+        if (nums[mid] <= nums[mid + 1]) {
+            left = mid + 1;
+        } else if (nums[mid] <= nums[mid - 1]) {
+            right = mid - 1;
+        } else {
+            return mid;
         }
-
-        // check if a candidate if valid
-        for (let i = 0; i < n; i++) {
-            if (i !== candidate && (!knows(i, candidate) || knows(candidate, i))) {
-                return -1;
-            }
-        }
-
-        return candidate;
-    };
+    }
+    return left;
 };
