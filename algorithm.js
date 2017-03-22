2e445932271f4e910d4f70086f919a6e5c639541
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-03-21 22:06:00 -0700

    isBadVersion algo added

--- algorithm.js
+++ algorithm.js
@@ -827,28 +827,26 @@ function solution(A, B, M, X, Y) {
 
 solution(A, B, M, X, Y)
 
-var findPeakElement = function(nums) {
-    var len = nums.length,
-        left,
-        right,
-        mid;
+var solution = function(isBadVersion) {
+                /**
+                 * @param {integer} n Total versions
+                 * @return {integer} The first bad version
+                 */
+                return function(n) {
+                    var start = 1,
+                        end = n,
+                        mid = parseInt(n/2);
 
-    if (len === 1) {
-        return 0;
-    }
+                    while (start < end) {
+                        if (isBadVersion(mid)) {
+                            end = mid;
+                        } else {
+                            start = mid + 1;
+                        }
 
-    left = 0;
-    right = len - 1;
-    while(left < right) {
-        mid = parseInt((left + right)/2);
+                        mid = parseInt((start + end)/2);
+                    }
 
-        if (nums[mid] <= nums[mid + 1]) {
-            left = mid + 1;
-        } else if (nums[mid] <= nums[mid - 1]) {
-            right = mid - 1;
-        } else {
-            return mid;
-        }
-    }
-    return left;
-};
+                    return start;
+                };
+            };
