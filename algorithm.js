5ef752def4446e28ece4f126c95f2ee90b774d34
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2017-03-21 22:08:35 -0700

    firstMissinPositive algo edited

--- algorithm.js
+++ algorithm.js
@@ -838,8 +838,7 @@ var firstMissingPositive = function(nums) {
                 break;
             }
             temp = nums[nums[i] - 1];
-            nums[nums[i] - 1] = nums[i];
-            nums[i] = temp;
+
         }
     }
 
