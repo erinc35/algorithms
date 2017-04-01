05428dd71f291e0f98eba2e19e2e98613b0cfe40
Erinç Emer <erincemer@gmail.com>
2017-04-01 13:25:43 -0700

    sortingColors algo added

--- algorithm.js
+++ algorithm.js
@@ -1273,31 +1273,29 @@ var singleNumber = function(nums) {
   return parseInt(Object.keys(hash))
 };
 
-var maxSlidingWindow = function(nums, k) {
-    var result = [],
-        queue = [],
-        len = nums.length,
+var sortColors = function(nums) {
+    var len = nums.length,
+        redEnds = 0,
+        blueStarts = len - 1,
         i;
 
-    if (k > len || k === 0) {
-        return result;
+    function swap(m, n, arr) {
+        var temp = arr[m];
+
+        arr[m] = arr[n];
+        arr[n] = temp;
     }
 
-    for (i = 0; i < len; i++) {
-        while (queue.length > 0 && nums[queue[queue.length - 1]] < nums[i]) {
-            queue.pop();
-        }
-
-        if (queue[0] < i - k + 1) {
-            queue.shift();
-        }
-
-        queue.push(i);
-
-        if (i >= k - 1) {
-            result.push(nums[queue[0]]);
+    for (i = 0; i <= blueStarts;) {
+        if (nums[i] === 0) {
+            swap(i, redEnds, nums);
+            i++;
+            redEnds++;
+        } else if (nums[i] === 2) {
+            swap(i, blueStarts, nums);
+            blueStarts--;
+        } else {
+            i++;
         }
     }
-
-    return result;
 };
