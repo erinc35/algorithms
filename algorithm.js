ca65a048a1b8e86b5d4a61c05ef5c161e71096d3
Erinç Emer <erincemer@gmail.com>
2017-04-05 15:03:15 -0700

    wallsandgates algo added

--- algorithm.js
+++ algorithm.js
@@ -1289,45 +1289,48 @@ var singleNumber = function(nums) {
 
 ////
 
-var partition = function(head, x) {
-    var cur = head,
-        next,
-        preHead,
-        preTail,
-        afterHead,
-        afterTail;
-
-    if (head === null) {
-        return null;
+var wallsAndGates = function(rooms) {
+    let queue = [];
+    let rowLen = rooms.length;
+    const MAX_VALUE = 2147483647;
+    if (rowLen === 0) {
+        return;
     }
 
-    while(cur) {
-        next = cur.next;
-        cur.next = null;
-        if (cur.val < x) {
-            if (!preHead) {
-                preHead = cur;
-                preTail = cur;
-            } else {
-                preTail.next = cur;
-                preTail = cur;
-            }
-        } else {
-            if (!afterHead) {
-                afterHead = cur;
-                afterTail = cur;
-            } else {
-                afterTail.next = cur;
-                afterTail = cur;
+    let colLen = rooms[0].length;
+
+    for (let i = 0; i < rowLen; i++) {
+        for (let j = 0; j < colLen; j++) {
+            if (rooms[i][j] === 0) {
+                queue.push([i, j]);
             }
         }
-        cur = next;
     }
 
-    if (preTail) {
-        preTail.next = afterHead;
-        return preHead;
-    } else {
-        return afterHead;
+    while (queue.length > 0) {
+        const cur = queue.shift();
+        const row = cur[0];
+        const col = cur[1];
+        const val = rooms[row][col];
+
+        if (row + 1 < rowLen && rooms[row + 1][col] === MAX_VALUE) {
+            rooms[row + 1][col] = val + 1;
+            queue.push([row + 1, col])
+        }
+
+        if (row - 1 >= 0 && rooms[row - 1][col] === MAX_VALUE) {
+            rooms[row - 1][col] = val + 1;
+            queue.push([row - 1, col]);
+        }
+
+        if (col + 1 < colLen && rooms[row][col + 1] === MAX_VALUE) {
+            rooms[row][col + 1] = val + 1;
+            queue.push([row, col + 1]);
+        }
+
+        if (col - 1 >= 0 && rooms[row][col - 1] === MAX_VALUE) {
+            rooms[row][col - 1] = val + 1;
+            queue.push([row, col - 1]);
+        }
     }
 };
