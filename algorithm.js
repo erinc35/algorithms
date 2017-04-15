8a2629bb2feb98e6ce86093d16af805db031b26a
Erinç Emer <erincemer@gmail.com>
2017-04-15 11:01:58 -0700

    build tree algo added

--- algorithm.js
+++ algorithm.js
@@ -1435,7 +1435,25 @@ function findDepth(root) {
 }
 ////
 
-var bulbSwitch = function(n) {
-    return Math.floor(Math.sqrt(n));
+var buildTree = function(inorder, postorder) {
+    var iLen = inorder.length,
+        pLen = postorder.length;
+
+    return getRoot(inorder, postorder, 0, iLen - 1, 0, pLen - 1);
 };
+
+function getRoot(inorder, postorder, iStart, iEnd, pStart, pEnd) {
+    if (iStart > iEnd || pStart > pEnd) {
+        return null;
+    }
+
+    var value = postorder[pEnd],
+        node = new TreeNode(value),
+        index = inorder.indexOf(value);
+
+    node.left = getRoot(inorder, postorder, iStart, index - 1, pStart, pStart + index - iStart - 1);
+    node.right = getRoot(inorder, postorder, index + 1, iEnd, pStart + index - iStart, pEnd - 1);
+
+    return node;
+}
 
