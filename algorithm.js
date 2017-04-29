2e6b4801fb2702f392ff2955eb5efedf9ba1b045
Erinç Emer <erincemer@gmail.com>
2017-04-29 13:23:39 -0700

    str to int algo added

--- algorithm.js
+++ algorithm.js
@@ -1612,27 +1612,59 @@ function main() {
 
 main();
 ///
-var subsets = function(nums) {
-    var result = [],
-        len = nums.length;
+var myAtoi = function(str) {
+    var len = str.length,
+        isNegative = null,
+        result = 0,
+        numberStarted = false,
+        cur,
+        i;
 
-    nums.sort(function(a, b) {
-        return a - b;
-    });
+    for (i = 0; i < len; i++) {
+        if (str.charAt(i) === ' ') {
+            if (!numberStarted) {
+                continue;
+            } else {
+                return getResult(isNegative, result);
+            }
+        }
 
-    helper(nums, 0, len - 1, [], result);
+        if (isNegative === null) {
+            if (str.charAt(i) === '-') {
+                isNegative = true;
+                numberStarted = true;
+                continue;
+            } else if (str.charAt(i) === '+') {
+                isNegative = false;
+                numberStarted = true;
+                continue;
+            }
+        }
+
+        cur = parseInt(str.charAt(i));
+
+        if (!isNaN(cur)) {
+            result = result*10 + cur;
+
+            if (!numberStarted) {
+                numberStarted = true;
+            }
+        } else {
+            return getResult(isNegative, result);
+        }
+    }
+
+    return getResult(isNegative, result);
+};
+// :(
+function getResult(isNegative, result) {
+    result = isNegative? result * (-1) : result;
+
+    if (result > 2147483647) {
+        result = 2147483647;
+    } else if (result < -2147483648) {
+        result = -2147483648;
+    }
 
     return result;
-};
-
-function helper(nums, start, end, curArr, result) {
-    result.push(curArr);
-
-    var i;
-
-    for (i = start; i <= end; i++) {
-        curArr.push(nums[i]);
-        helper(nums, i + 1, end, curArr.concat(), result);
-        curArr.pop();
-    }
 }
