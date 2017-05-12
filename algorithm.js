ddc30d0514c7a71ad837594ac022fa35ea47171a
Erinç Emer <erincemer@gmail.com>
2017-05-12 08:00:45 -0700

    count primes algo added

--- algorithm.js
+++ algorithm.js
@@ -1694,6 +1694,26 @@ function uniq(arr){
   return dupless;
 }
 ///
-var detectCapitalUse = function(word) {
-    return /^[A-Z]?([a-z]*|[A-Z]*)$/.test(word);
+var countPrimes = function(n) {
+  var isPrime = [];
+  for (var i = 2; i < n; i++) {
+    isPrime[i] = true;
+  }
+
+  for (var i = 2; i * i < n; i++) {
+    if (isPrime[i]) {
+      for (var j = i * i; j < n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  var count = 0;
+  for (var i = 2; i < n; i++) {
+    if (isPrime[i]) {
+      count++;
+    }
+  }
+
+  return count;
 };
