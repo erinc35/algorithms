aeadd6bf1dc2168ecf9506beb78529758fadbd82
Erinç Emer <erincemer@gmail.com>
2017-05-17 10:07:17 -0700

    first bad version algo added

--- algorithm.js
+++ algorithm.js
@@ -1694,47 +1694,26 @@ function uniq(arr){
   return dupless;
 }
 ///
-var isInterleave = function(s1, s2, s3) {
-    let dp = [];
-    const len1 = s1.length;
-    const len2 = s2.length;
-    const len3 = s3.length;
+var solution = function(isBadVersion) {
+                /**
+                 * @param {integer} n Total versions
+                 * @return {integer} The first bad version
+                 */
+                return function(n) {
+                    var start = 1,
+                        end = n,
+                        mid = parseInt(n/2);
 
-    if (len1 + len2 !== len3) {
-        return false;
-    }
+                    while (start < end) {
+                        if (isBadVersion(mid)) {
+                            end = mid;
+                        } else {
+                            start = mid + 1;
+                        }
 
-    for (let i = 0; i <= len1; i++) {
-        const arr = new Array(len2 + 1).fill(false);
-        dp.push(arr);
-    }
+                        mid = parseInt((start + end)/2);
+                    }
 
-    dp[0][0] = true;
-
-    for (let i = 1; i <= len1; i++) {
-        dp[i][0] = s1.charAt(i - 1) === s3.charAt(i - 1);
-
-        if (!dp[i][0]) {
-            break;
-        }
-    }
-
-    for (let j = 1; j <= len2; j++) {
-        dp[0][j] = s2.charAt(j - 1) === s3.charAt(j - 1);
-
-        if (!dp[0][j]) {
-            break;
-        }
-    }
-
-    for (let i = 1; i <= len1; i++) {
-        for (let j = 1; j <= len2; j++) {
-            let k = i + j - 1;
-
-            dp[i][j] = (dp[i][j - 1] && (s2.charAt(j - 1) === s3.charAt(k))) || (dp[i - 1][j] && (s1.charAt(i - 1) === s3.charAt(k)));
-        }
-    }
-
-    return dp[len1][len2];
-};
-
+                    return start;
+                };
+            };
