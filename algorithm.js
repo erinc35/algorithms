e6904a8a7613d6c53709ec703eaa648f287222ce
Erinç Emer <erincemer@gmail.com>
2017-05-19 11:59:44 -0700

    min stack algo added

--- algorithm.js
+++ algorithm.js
@@ -1694,61 +1694,54 @@ function uniq(arr){
   return dupless;
 }
 ///
-var maximumGap = function(nums) {
-    var length = nums.length,
-        bucket = [],
-        max = Number.MIN_VALUE,
-        min = Number.MAX_VALUE,
-        gap = 0,
-        bLen,
-        bIndex,
-        prev,
-        i;
+var MinStack = function() {
+    this.min = [];
+    this.arr = [];
+};
 
-    if (length < 2) {
-        return 0;
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+MinStack.prototype.push = function(x) {
+    var len = this.min.length;
+    if (this.arr.length === 0) {
+        this.min.push(x);
+    } else if (this.min[len - 1] >= x) {
+        this.min.push(x);
+    }
+    this.arr.push(x);
+};
+
+/**
+ * @returns {void}
+ */
+MinStack.prototype.pop = function() {
+    var elem,
+        len = this.min.length;
+    if (this.arr.length > 0) {
+        elem = this.arr.pop();
+    }
+    if (elem === this.min[len - 1]) {
+        this.min.pop();
+    }
+};
+
+/**
+ * @returns {number}
+ */
+MinStack.prototype.top = function() {
+    var len = this.arr.length;
+    if (len > 0) {
+        return this.arr[len - 1];
     }
 
-    for (i = 0; i < length; i++) {
-        if (nums[i] > max) {
-            max = nums[i];
-        }
+};
 
-        if (nums[i] < min) {
-            min = nums[i];
-        }
-    }
-
-    // length of each bucket
-    bLen = Math.floor((max - min) / length) + 1;
-
-    for (i = 0; i < length; i++) {
-        bIndex = Math.floor((nums[i] - min) / bLen);
-
-        if (!bucket[bIndex]) {
-            bucket[bIndex] = [];
-            bucket[bIndex].push(nums[i]);
-            bucket[bIndex].push(nums[i]);
-        } else {
-            if (nums[i] < bucket[bIndex][0]) {
-                bucket[bIndex][0] = nums[i];
-            } else if (nums[i] > bucket[bIndex][1]) {
-                bucket[bIndex][1] = nums[i];
-            }
-        }
-    }
-
-    length = bucket.length;
-    prev = 0;
-
-    for (i = 1; i < length; i++) {
-        if (!bucket[i]) {
-            continue;
-        }
-
-        gap = Math.max(gap, bucket[i][0] - bucket[prev][1]);
-        prev = i;
-    }
-
-    return gap;
+/**
+ * @returns {number}
+ */
+MinStack.prototype.getMin = function() {
+    var len = this.min.length;
+    return this.min[len - 1];
 };
