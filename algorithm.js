41acefd262a5c30a10e8f62ca128d217b1da0626
Erinç Emer <erincemer@gmail.com>
2017-05-20 07:52:41 -0700

    rotate image algo added

--- algorithm.js
+++ algorithm.js
@@ -1694,31 +1694,22 @@ function uniq(arr){
   return dupless;
 }
 ///
-var subsetsWithDup = function(nums) {
-    var result = [],
-        len = nums.length;
+var rotate = function(matrix) {
+    var len = matrix.length,
+        i,
+        level,
+        tmp;
 
-    nums.sort(function(a, b) {
-        return a - b;
-    });
+    level = 0;
 
-    helper(nums, 0, len - 1, [], result);
-
-    return result;
-};
-
-function helper(nums, start, end, curArr, result) {
-    result.push(curArr);
-
-    var i;
-
-    for (i = start; i <= end; i++) {
-        // skip duplicates
-        if (i > start && nums[i] === nums[i - 1]) {
-            continue;
+    while (level < len/2) {
+        for (i = level; i < len - level - 1; i++) {
+           tmp = matrix[level][i];
+           matrix[level][i] = matrix[len - i - 1][level];
+           matrix[len - i - 1][level] = matrix[len - 1 - level][len - 1 - i];
+           matrix[len - 1 - level][len - 1 - i] = matrix[i][len - 1 - level];
+           matrix[i][len - 1 - level] = tmp;
         }
-        curArr.push(nums[i]);
-        helper(nums, i + 1, end, curArr.concat(), result);
-        curArr.pop();
+        level++;
     }
-}
+};
