001d01f78baa5067283c23f738a9e5977db0c197
Erinç Emer <erincemer@gmail.com>
2017-06-01 07:30:39 -0700

    min path sum algo added

--- algorithm.js
+++ algorithm.js
@@ -1717,22 +1717,38 @@ var climbStairs = function(n) {
    return result
 };
 ////
-var moveZeroes = function(nums) {
-    var len = nums.length,
-        curIndex = 0,
-        i;
+var minPathSum = function(grid) {
+    var result = [],
+        rowL = grid.length,
+        columnL,
+        i,
+        j;
 
-    for (i = 0; i < len; i++) {
-        if (nums[i] === 0) {
-            continue;
+    if (rowL === 0) {
+        return result;
+    }
+
+    columnL = grid[0].length;
+
+    for (i = 0; i < rowL; i++) {
+        result.push(new Array(columnL));
+    }
+
+    result[0][0] = grid[0][0];
+
+    for (i = 1; i < columnL; i++) {
+        result[0][i] = grid[0][i] + result[0][i - 1];
+    }
+
+    for (i = 1; i < rowL; i++) {
+        result[i][0] = grid[i][0] + result[i - 1][0];
+    }
+
+    for (i = 1; i < rowL; i++) {
+        for (j = 1; j < columnL; j++) {
+            result[i][j] = Math.min(result[i - 1][j], result[i][j - 1]) + grid[i][j];
         }
-
-        nums[curIndex] = nums[i];
-        curIndex++;
     }
 
-    for (i = curIndex; i < len; i++) {
-        nums[i] = 0;
-    }
+    return result[rowL - 1][columnL - 1];
 };
-
