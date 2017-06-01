2d8059e7f44f58508691d85863d64a1d366e45e4
Erinç Emer <erincemer@gmail.com>
2017-06-01 07:30:05 -0700

    move zeroes 2 algo added

--- algorithm.js
+++ algorithm.js
@@ -1717,47 +1717,22 @@ var climbStairs = function(n) {
    return result
 };
 ////
-var numIslands = function(grid) {
-    var len = grid.length,
-        result = 0,
-        len1,
-        i,
-        j;
-
-    if (len === 0) {
-        return 0;
-    }
-
-    len1 = grid[0].length;
-
-    if (len1 === 0) {
-        return 0;
-    }
+var moveZeroes = function(nums) {
+    var len = nums.length,
+        curIndex = 0,
+        i;
 
     for (i = 0; i < len; i++) {
-        for (j = 0; j < len1; j++) {
-            if (grid[i][j] !== '1') {
-                continue;
-            }
-
-            result++;
-            dfs(grid, i, j, len, len1);
+        if (nums[i] === 0) {
+            continue;
         }
+
+        nums[curIndex] = nums[i];
+        curIndex++;
     }
 
-    return result;
+    for (i = curIndex; i < len; i++) {
+        nums[i] = 0;
+    }
 };
 
-function dfs(grid, i, j, len, len1) {
-    if (i >= len || j >= len1 || i < 0 || j < 0) {
-        return;
-    }
-
-    if (grid[i][j] === '1') {
-        grid[i][j] = '0';
-        dfs(grid, i - 1, j, len, len1);
-        dfs(grid, i, j - 1, len, len1);
-        dfs(grid, i + 1, j, len, len1);
-        dfs(grid, i, j + 1, len, len1);
-    }
-}
