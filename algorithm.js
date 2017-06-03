d3d0df5808e79a5e5cf92fdbe8dba5d81323dbc6
Erinç Emer <erincemer@gmail.com>
2017-06-03 08:19:11 -0700

    trapping rain water algo added

--- algorithm.js
+++ algorithm.js
@@ -1718,33 +1718,35 @@ var climbStairs = function(n) {
    return result
 };
 ////
-ar swapPairs = function(head) {
-    if (head === null) {
-        return null;
-    }
+var trap = function(height) {
+    var len = height.length,
+        leftMax = 0,
+        rightMax = 0,
+        left = 0,
+        right = len - 1,
+        sum = 0;
 
-    var next = head.next,
-        result = next? next : head,
-        prevTail,
-        curHead,
-        nextHead;
+    while (left <= right) {
+        if (height[left] <= height[right]) {
+            if (height[left] > leftMax) {
+                leftMax = height[left];
+            } else {
+                sum += leftMax - height[left];
+            }
 
-    curHead = head;
+            left++;
+        } else {
+            if (height[right] > rightMax) {
+                rightMax = height[right];
+            } else {
+                sum += rightMax - height[right];
+            }
 
-    while(next) {
-        nextHead = next.next;
-
-        if (prevTail) {
-            prevTail.next = next;
+            right--;
         }
-
-        next.next = curHead;
-        curHead.next = nextHead;
-        prevTail = curHead;
-
-        curHead = nextHead;
-        next = curHead && curHead.next;
     }
 
-    return result;
+    return sum;
 };
+
+
