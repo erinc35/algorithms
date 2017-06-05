ac09428c395138b55a866a2b688484ebba456db3
Erinç Emer <erincemer@gmail.com>
2017-06-04 21:13:27 -0700

    gray code algo added

--- algorithm.js
+++ algorithm.js
@@ -1718,91 +1718,23 @@ var climbStairs = function(n) {
    return result
 };
 ////
-var wiggleSort = function(nums) {
-    var len = nums.length,
-        temp = [],
-        midIndex = parseInt((len + 1) / 2),
-        i,
-        l,
-        r,
-        medium;
-
-    medium = findKth(0, len - 1, midIndex, nums);
-
-    for (i = 0, l = 0, r = len - 1; i < len; i++) {
-        if (nums[i] < medium) {
-            temp[l++] = nums[i];
-        } else if (nums[i] > medium) {
-            temp[r--] = nums[i];
-        }
-    }
-
-    while (l < midIndex) {
-        temp[l++] = medium;
-    }
-
-    while (r >= midIndex) {
-        temp[r--] = medium;
-    }
-
-    r = len;
-
-    for (i = 0; i < len; i++) {
-        nums[i] = (i & 1) === 0? temp[--l] : temp[--r];
-    }
-};
-
-function findKth(start, end, k, nums) {
-    if (start >= end) {
-        return nums[end];
-    }
-
-    var index = partition(start, end, nums),
-        count = index - start + 1;
-
-    if (count === k) {
-        return nums[index];
-    }
-
-    if (count < k) {
-        return findKth(index + 1, end, k - count, nums);
-    }
-
-    return findKth(start, index - 1, k, nums);
-}
-
-function partition(start, end, nums) {
-    var val = nums[start],
+var grayCode = function(n) {
+    var result = [],
+        cur,
         i,
         j;
 
-    i = start + 1;
-    j = end;
+    result [0] = 0;
 
-    while (true) {
-        while (nums[i] <= val) {
-            i++;
-        }
-
-        while (nums[j] > val) {
-            j--;
-        }
-
-        if (i >= j) {
-            break;
-        }
-
-        swap(nums, i, j);
+    if (n === 0) {
+        return result;
     }
 
-    swap(nums, j, start);
+    for (i = 1; i <= n; i++) {
+        for (j = Math.pow(2, i - 1); j > 0; j--) {
+            result.push(result[j - 1] + (1 << (i - 1)));
+        }
+    }
 
-    return j;
-}
-
-function swap(nums, i, j) {
-    var temp = nums[i];
-
-    nums[i] = nums[j];
-    nums[j] = temp;
-}
+    return result;
+};
