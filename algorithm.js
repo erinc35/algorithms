cac1b968485224b726b306d016c2080ee33ffa6b
Erinç Emer <erincemer@gmail.com>
2017-06-20 17:21:09 -0700

    house robber algo alt. added

--- algorithm.js
+++ algorithm.js
@@ -1776,3 +1776,28 @@ var rob = function(nums) {
     }
     return Math.max(a,b)
 };
+
+//alternative
+
+var rob = function(nums) {
+    var len = nums.length,
+        result = [],
+        i;
+
+    if (len === 0) {
+        return 0;
+    }
+    if (len === 1) {
+        return nums[0];
+    }
+
+    result[0] = nums[0];
+    result[1] = Math.max(nums[1], nums[0]);
+    for(i = 2; i < len; i++) {
+        result[i] = Math.max(result[i - 2] + nums[i], result[i - 1]);
+        console.log(result)
+    }
+
+    return result[len - 1];
+};
+
