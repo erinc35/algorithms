21b8f45d2b942a0c4f708519b8b007515d4490be
Erinc Emer <erincemer@gmail.com>
2019-10-20 09:24:59 -0700

    binary search added

--- algorithm.js
+++ algorithm.js
@@ -1868,3 +1868,31 @@ var singleNonDuplicate = function (nums) {
 };
 
 singleNonDuplicate([1, 1, 2, 2, 3, 3, 4, 8, 8])
+
+//find the target with a binary search
+
+function binarySearch(list, value) {
+    // initial values for start, middle and end
+    let start = 0
+    let stop = list.length - 1
+    let middle = Math.floor((start + stop) / 2)
+
+    // While the middle is not what we're looking for and the list does not have a single item
+    while (list[middle] !== value && start < stop) {
+        if (value < list[middle]) {
+            stop = middle - 1
+        } else {
+            start = middle + 1
+        }
+
+        // recalculate middle on every iteration
+        middle = Math.floor((start + stop) / 2)
+    }
+
+    // if the current middle item is what we're looking for return it's index, else return -1
+    return (list[middle] !== value) ? -1 : middle
+}
+
+const list = [2, 5, 8, 9, 13, 45, 67, 99]
+
+//
