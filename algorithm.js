4bf6a05515cca9630f61f0d8680874b2e68028d7
Erinç Emer <erincemer@gmail.com>
2019-12-11 12:30:29 -0800

    better impl of url decoding

--- algorithm.js
+++ algorithm.js
@@ -1962,3 +1962,30 @@ var search = function(nums, target) {
 };
 
 search([2,5,6,0,0,1,2], 3)
+
+//better url param decoding
+function getUrlParams(url) {
+  // http://stackoverflow.com/a/23946023/2407309
+  if (typeof url == 'undefined') {
+    url = window.location.search
+  }
+  var url = url.split('#')[0] // Discard fragment identifier.
+  var urlParams = {}
+  var queryString = url.split('?')[1]
+  // console.log(queryString) //product=shirt&color=blue&newuser&size=m
+  if (!queryString) {
+    if (url.search('=') !== false) {
+      queryString = url
+    }
+  } else {
+    var keyValuePairs = queryString.split('&')
+    for (var i = 0; i < keyValuePairs.length; i++) {
+      var keyValuePair = keyValuePairs[i].split('=')
+      console.log(keyValuePair)
+      if (keyValuePair[1]) urlParams[keyValuePair[0]] = keyValuePair[1]
+    }
+  }
+  return urlParams
+}
+getUrlParams('http://example.com/?product=shirt&color=blue&newuser&size=m')
+///
