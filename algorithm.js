e54e9b600c0821945e1d9788dd6af5268cc073a7
Erinç Emer <erincemer@gmail.com>
2020-01-17 18:46:20 -0800

    set zeroes in matrix added

--- algorithm.js
+++ algorithm.js
@@ -2151,3 +2151,41 @@ function findMatrix(matrix, n) {
   }
   return 'not found'
 }
+///set zerioes in matrix
+
+var setZeroes = function(matrix) {
+    let colZero = false;
+    for (let i=0;i<matrix.length;i++) {
+
+        if (matrix[i][0]==0) {
+            colZero = true;
+        }
+
+        for (let j=1;j<matrix[0].length;j++) {
+            if (matrix[i][j]==0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    for (let i=1;i<matrix.length;i++) {
+        for (let j=1;j<matrix[0].length;j++) {
+            if (matrix[i][0]==0 || matrix[0][j]==0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (colZero) {
+        for (let i=0;i<matrix.length;i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    if (matrix[0][0]==0) {
+        for (let i=0;i<matrix[0].length;i++) {
+            matrix[0][i] = 0;
+        }
+    }
+}
